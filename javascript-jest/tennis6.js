06e92ba352bc788f720060ec10819ce48e500fb0
WongYC-66 <yengchien@hotmal.com>
2024-12-19 15:32:58 +0800

    refactor: tennis6

--- javascript-jest/tennis6.js
+++ javascript-jest/tennis6.js
@@ -1,92 +1,33 @@
 'use strict';
 
+const scoreToText = {
+  '0': 'Love',
+  '1': 'Fifteen',
+  '2': 'Thirty',
+  '3': 'Forty',
+}
+
 const getScore = (player1Score, player2Score) => {
-    let result;
+  let result;
 
-    if (player1Score === player2Score)
-    {
-      // tie score
-      let tieScore;
-      switch (player1Score)
-      {
-        case 0:
-          tieScore = "Love-All";
-          break;
-        case 1:
-          tieScore = "Fifteen-All";
-          break;
-        case 2:
-          tieScore = "Thirty-All";
-          break;
-        default:
-          tieScore = "Deuce";
-          break;
-      }
-
-      result = tieScore;
-    }
-    else if (player1Score >= 4 || player2Score >= 4)
-    {
-      // end-game score
-      let endGameScore;
-
-      if (player1Score - player2Score === 1) {
-        endGameScore = "Advantage " + "player1";
-      } else if (player1Score - player2Score === -1) {
-        endGameScore = "Advantage " + "player2";
-      } else if (player1Score - player2Score >= 2) {
-        endGameScore = "Win for " + "player1";
-      } else {
-        endGameScore = "Win for " + "player2";
-      }
-
-      result = endGameScore;
-    }
-    else
-    {
-      // regular score
-      let regularScore;
-
-      let score1;
-
-      switch (player1Score) {
-        case 0:
-          score1 = "Love";
-          break;
-        case 1:
-          score1 = "Fifteen";
-          break;
-        case 2:
-          score1 = "Thirty";
-          break;
-        default:
-          score1 = "Forty";
-          break;
-      }
-
-      let score2;
-      switch (player2Score) {
-        case 0:
-          score2 = "Love";
-          break;
-        case 1:
-          score2 = "Fifteen";
-          break;
-        case 2:
-          score2 = "Thirty";
-          break;
-        default:
-          score2 = "Forty";
-          break;
-      }
-
-      regularScore = score1 + "-" + score2;
-
-      result = regularScore;
-    }
-
-    return result;
-
+  if (player1Score === player2Score) {
+    let tieScore = player1Score > 2
+      ? "Deuce"
+      : `${scoreToText[player1Score]}-All`
+    result = tieScore;
+  }
+  else if (player1Score >= 4 || player2Score >= 4) {
+    let diff = player1Score - player2Score
+    let endGameScore = Math.abs(diff) === 1
+      ? `Advantage ${diff > 0 ? 'player1' : 'player2'}`
+      : `Win for ${diff > 0 ? 'player1' : 'player2'}`
+    result = endGameScore;
+  }
+  else {
+    let regularScore = scoreToText[player1Score] + "-" + scoreToText[player2Score];
+    result = regularScore;
+  }
+  return result;
 }
 
 module.exports = getScore;
