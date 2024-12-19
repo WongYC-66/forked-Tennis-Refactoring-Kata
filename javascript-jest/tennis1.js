6b8154431b9791810ce752806b9be374f73dc70d
WongYC-66 <yengchien@hotmal.com>
2024-12-19 12:27:53 +0800

    refactor : tennis1

--- javascript-jest/tennis1.js
+++ javascript-jest/tennis1.js
@@ -1,51 +1,29 @@
 'use strict';
 
+const scoreToText = {
+    '0': 'Love',
+    '1': 'Fifteen',
+    '2': 'Thirty',
+    '3': 'Forty',
+}
+
 function getScore(m_score1, m_score2) {
     var score = "";
-    var tempScore = 0;
     if (m_score1 === m_score2) {
-        switch (m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
+        if (m_score1 <= 2) {
+            score = `${scoreToText[m_score1]}-All`;
+        } else {
+            score = "Deuce"
         }
     } else if (m_score1 >= 4 || m_score2 >= 4) {
-        var minusResult = m_score1 - m_score2;
-        if (minusResult === 1) {score = "Advantage player1";}
-        else if (minusResult === -1) {score = "Advantage player2";}
-        else if (minusResult >= 2) {score = "Win for player1";}
-        else {score = "Win for player2";}
-    } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) {tempScore = m_score1;}
-            else {
-                score += "-";
-                tempScore = m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
+        let diff = Math.abs(m_score1 - m_score2)
+        if (m_score1 > m_score2) {
+            score = diff >= 2 ? "Win for player1" : "Advantage player1"
+        } else {
+            score = diff >= 2 ? "Win for player2" : "Advantage player2"
         }
+    } else {
+        score = `${scoreToText[m_score1]}-${scoreToText[m_score2]}`
     }
     return score;
 }
